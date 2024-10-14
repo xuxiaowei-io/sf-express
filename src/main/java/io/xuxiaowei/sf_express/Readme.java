f0c7e31a8ea07c873128eb5bc52fbff666f5cd79
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-10-14 11:54:12 +0800

    :bookmark: V2.1.7

---
+++ src/main/java/io/xuxiaowei/sf_express/Readme.java
@@ -0,0 +1,11 @@
+package io.xuxiaowei.sf_express;
+
+/**
+ * 顺风快递API-JAVA-SDK：此项目仅仅是将 顺风 SDK 上传至中央仓库
+ *
+ * @author xuxiaowei
+ * @see <a href="https://qiao.sf-express.com/">顺风快递 SDK 下载地址</a>
+ */
+public class Readme {
+
+}
